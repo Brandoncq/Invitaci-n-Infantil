12a602efe1a37fcaada4701a92a06f21c6e0dec5
Davis <brandoncq036@hotmail.com>
2025-02-24 10:12:40 -0500

    Arreglando 7

--- src/app/peppa-pig/layout.tsx
+++ src/app/peppa-pig/layout.tsx
@@ -23,5 +23,5 @@ export default function InvitationLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main className="w-full min-h-lvh m-0 p-0">{children}</main>;
+  return <div className="w-full min-h-lvh m-0 p-0">{children}</div>;
 }
