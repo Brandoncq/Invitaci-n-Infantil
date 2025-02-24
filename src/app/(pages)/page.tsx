382061ce0fc39890f09b8ebf98641ef237604c20
Davis <brandoncq036@hotmail.com>
2025-02-24 10:00:40 -0500

    Arreglando 6

--- src/app/(pages)/page.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-export default function Home() {
-  return (
-    <div className="w-ful h-lvh">
-      <Link href={"melissa-eduardo"}>melissa-eduardo</Link>
-    </div>
-  );
-}
